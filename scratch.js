ecf03a3fed0c54137e1d4d6c7de672f25d21b9b8
Natalie Childs <nata.faye@gmail.com>
2025-08-30 10:54:30 -0700

    NASA api

--- scratch.js
+++ scratch.js
@@ -1,27 +1,43 @@
+// REST API - the standard (this is what we're teaching)
+// SOAP API - this is really old
+// GraphQL API - this is really new and more niche
 
-async function getMoves() {
-    try {
-        // Make a request to the API and wait for the response
-        const response = await fetch("https://pokeapi.co/api/v2/move/3")
-        // parse the data in the body of the response from JSON into javascript objects/arrays
-        const data = await response.json()
-        console.log(data)
-    } catch (error) {
-        alert("There was an error!")
-    }
+
+async function fetchData() {
+    // fetch("https://developer.nps.gov/api/v1/events?stateCode=NV&api_key=" + NPS_KEY)
+    // console.log("This logs before we get the response from nps.gov")
+
+    const promiseOfResponse = fetch("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=" + NASA_API_KEY)
+    console.log("This logs BEFORE we get the response from nasa.gov Promise of Response:", promiseOfResponse)
 }
 
-async function getLego() {
-    try {
-        const response = await fetch("https://rebrickable.com/api/v3/lego/sets?key=" + LEGO_KEY)
-        const data = await response.json()
-        console.log(data)
-    } catch (error) {
-        alert("There was an error!")
-    }
+async function fetchDataWait() {
+    const response = await fetch("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1500&api_key=" + NASA_API_KEY)
+    // If you call an async function and don't wait, you get a promise (little buzzer)
+    console.log("This logs AFTER we get the response from nasa.gov Response:", response)
+    const data = await response.json(); // parses it from a string of JSON to actual Javascript objects and arrays
+    renderFirstPhoto(data)
 }
 
+// async function fetchSomething() {
+//     try {
+//         const response = await fetch("fdsfdsf") // PUT YOUR URL HERE
+//         if(!response.ok) {
+//             alert("Uh oh! Is the server down?")
+//         }
+//         const data = await response.json()
+//         // Do something with the data
+//         renderFirstPhoto(data)
+//     } catch (error) {
+//         alert("Oh no! Is your internet out?")
+//     }
+// }
 
-// What are .env files?
-// They allow you to have different values for variables in the dev version of the app vs the actual app
-// For example, make requests to a test database during development and the actual database in the real app
+const resultsDiv = document.getElementById("results")
+
+function renderFirstPhoto(data) {
+    const newImage = document.createElement("img")
+    newImage.src = data.photos[0].img_src
+
+    resultsDiv.appendChild(newImage)
+}
